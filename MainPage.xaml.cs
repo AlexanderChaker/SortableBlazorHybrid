63c8efe44c77ee423b9b2893df611ed72e0a0270
alexander.chaker <atyahoo.com@hotmail.com>
2024-05-05 11:37:19 -0400

    Initial Blazor Hybrid Net9.0 proj

---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SortableBlazorHybrid;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SortableBlazorHybrid;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
